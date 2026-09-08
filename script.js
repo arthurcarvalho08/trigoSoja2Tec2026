41a76c2f224f7cd5b47b8c31cc0e0825aa65c090
arthurcarvalho08 <arthur.galego.carvalho@escola.pr.gov.br>
2026-09-08 09:31:10 -0300

    22

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const botaoTema = document.getElementById('botao-tema');
 
+botaoTema.addEventListener('click', () => {
+    document.body.classList.toggle('modo-escuro');
+    
+    if (document.body.classList.contains('modo-escuro')) {
+        botaoTema.textContent = '☀️ Tema';
+    } else {
+        botaoTema.textContent = '🌙 Tema';
+    }
+});
